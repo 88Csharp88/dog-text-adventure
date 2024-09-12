473decb665b51fd5ade172675d5b8f052b0815fe
88Csharp88 <70967753+88Csharp88@users.noreply.github.com>
2024-09-12 00:11:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ document.getElementById('address-form').addEventListener('submit', async (event)
         const apiKey = '27d72bfbe4aadbfe692a706949e97c88fb14607ebfccccb5e70945acdfa89b24'; // Put your API key here
         
         // Replace the URL with the correct Unisat API endpoint as per the documentation
-        //const response = await fetch(`https://unisat.io/api/address/${address}/runes`);
-        const response = await fetch('https://open-api.unisat.io/v1/indexer/address/${address}/runes/balance-list', {
+        //const response = await fetch(`https://unisat.io/api/address/${address}/runes`);  //${address}
+        const response = await fetch('https://open-api.unisat.io/v1/indexer/address/bc1q9qx2g6qt6kkvsthyl2kgyte5uqrqltqfpm7rp9/runes/balance-list', {
         method: 'GET',
         headers: {
                 'Accept': 'application/json',
