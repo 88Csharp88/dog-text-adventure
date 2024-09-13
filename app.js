3bb6cd581d937cde07db94612ae3611b207a9917
88Csharp88 <70967753+88Csharp88@users.noreply.github.com>
2024-09-12 23:34:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ document.getElementById('address-form').addEventListener('submit', async (event)
     event.preventDefault();
     const address = document.getElementById('unisat-address').value.trim();
     const resultDiv = document.getElementById('result');
-
-    // Clear previous result
-    //resultDiv.innerHTML = "Loading...";
-    //console.log("Loading...");
-    //console.log('Address entered:', address);
+   
     // Display the address captured from the input
     console.log(`Address entered: ${address}`);
     console.log(address.length);
@@ -17,22 +13,19 @@ document.getElementById('address-form').addEventListener('submit', async (event)
         // Log the address being used in the request
         console.log(`Fetching rune balance for address: ${address}`);
 
-        const apiKey = '27d72bfbe4aadbfe692a706949e97c88fb14607ebfccccb5e70945acdfa89b24'; // Put your API key here
-        
-        // Replace the URL with the correct Unisat API endpoint as per the documentation
-        //const response = await fetch(`https://unisat.io/api/address/${address}/runes`);  //${address}
+        const apiKey = '27d72bfbe4aadbfe692a706949e97c88fb14607ebfccccb5e70945acdfa89b24';
+         
         const generatedUrl = `https://open-api.unisat.io/v1/indexer/address/${address}/runes/balance-list`;
         console.log(`Generated URL: ${generatedUrl}`);
         
-        //const response = await fetch('https://open-api.unisat.io/v1/indexer/address/${address}/runes/balance-list', {
         const response = await fetch(generatedUrl, {
         method: 'GET',
         headers: {
                 'Accept': 'application/json',
-                'Authorization': `Bearer ${apiKey}` // Include your API key here
+                'Authorization': `Bearer ${apiKey}` 
         },
         });
-        //const data = await response.json();
+        
         // Log the response status
         console.log(`Response status: ${response.status}`);
 
