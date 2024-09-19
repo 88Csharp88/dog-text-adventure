cdd667060d457376959417781af32869e09d97df
88Csharp88 <70967753+88Csharp88@users.noreply.github.com>
2024-09-18 22:09:00 -0500

    Update game.js

--- game.js
+++ game.js
@@ -117,19 +117,19 @@ function handleExDecision(level, hasLobo, choice) {
     if (choice === 'yes') {
         updateGameOutput('That was a great decision.  Now you have time to focus!');
         // Present attack options before simulating the fight
-        presentAttackOptions(level, hasLobo);
+       
     } else {
         //updateGameOutput('You decide not to log onto X. Go back to bed and gather your strength...');
         presentChoice('You did not block your ex and now they arrive at your door. Prepare to fight!');
 
-         // Add the FUDer image
-        const fuderImage = `
+         // Add the Ex image
+        const ExImage = `
             <div>
-                <img src="https://github.com/88Csharp88/dog-text-adventure/blob/main/images/FUDer.png?raw=true" alt="FUDer" style="width: 200px; height: auto;"/>
+                <img src="https://github.com/88Csharp88/dog-text-adventure/blob/main/images/ex.jpg?raw=true" alt="Ex" style="width: 200px; height: auto;"/>
             </div>
         `;
         const gameOutput = document.getElementById('game-output');
-        gameOutput.innerHTML += fuderImage; // Add the image to the game output
+        gameOutput.innerHTML += ExImage; // Add the image to the game output
         
          // Present attack options before simulating the fight
         presentAttackOptions(level, hasLobo);
