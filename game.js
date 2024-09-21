85bc2a49f444c2a45ba56ae49e1df5efd41241f1
88Csharp88 <70967753+88Csharp88@users.noreply.github.com>
2024-09-20 23:38:50 -0500

    Update game.js

--- game.js
+++ game.js
@@ -118,7 +118,7 @@ function presentNewOptions(level, hasLobo, mana, maxMana) {
 const enemies = [
     { name: "FUDer", 
         image: "https://github.com/88Csharp88/dog-text-adventure/blob/main/images/FUDer.png?raw=true",
-        deadImage: ""
+        deadImage: "https://github.com/88Csharp88/dog-text-adventure/blob/testing-game2-of-2/images/Dead%20Cat.png?raw=true"
     },
     
     { name: "Gary Gensler", 
